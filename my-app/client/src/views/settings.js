34acbb2f3f151144ef1942aedb5b11f073e7bdae
sofia-bjorkesjo <sb223fz@student.lnu.se>
2018-02-27 16:13:17 +0100

    now I can choose organisations using checkboxes

--- my-app/client/src/views/settings.js
+++ my-app/client/src/views/settings.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import Links from './Links';
 import { RadioGroup, RadioButton } from 'react-radio-buttons';
+import { SSL_OP_CISCO_ANYCONNECT } from 'constants';
+import Checkbox from 'rc-checkbox'
 
 class Settings extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      orgs: []
-    }    
+      orgs: [],
+      disabled: false
+    }
+
   }
 
-  testar() {
+
+  renderOrganisations() {
 
     var organisations = localStorage.getItem('organisationer');
     var parsedOrgs = JSON.parse(organisations);
     var orgsArray = [];
     for(let i = 0; i < parsedOrgs.length; i++) {
-      orgsArray.push(<RadioButton key={[i]}value={parsedOrgs[i]}>
-      {parsedOrgs[i]}
-      </RadioButton>)
+
+      orgsArray.push(<p>
+        <label>
+            <Checkbox
+              defaultChecked
+              onChange={this.onChange}
+              disabled={this.state.disabled}
+            />
+            {parsedOrgs[i]}
+          </label>
+        </p>)      
     }
     return orgsArray
   }
@@ -34,59 +47,21 @@ class Settings extends Component {
     this.setState({orgs: orgsArray})
   }
 
-  // listOrganisations(orgs) {
-  //   orgs = this.props.orgs;
-  //   orgs.forEach(element => {
-  //     <label>
-  //        <input type="radio" value="test"/>
-  //             Test
-  //     </label>
-  //   });
-  // }
-  // componentDidMount() { 
-  //     fetch('https://api.github.com/user/orgs?access_token=' + this.state.response,{
-  //     method: 'GET'
-  //   })
-  //   .then(res => res.json())
-  //   .catch(error => console.log(error))
-  //   .then(response => console.log(response))
-  //   //.then(response => this.saveOrganizations(response))
-  // }
+  onChange(e) {
+    console.log('Checkbox checked:', (e.target.checked));
+  }
 
-
-
-  // getInitialState () {
-  //   return {
-  //     selectedOption: ''
-  //   }
-  // }
-
-  // handleOptionChange(changeEvent) {
-  //   this.setState({
-  //     selectedOption: changeEvent.currentTarget.value
-  //   })
-  // }
-
-  onChange(value) {
-    console.log(value)
+  save() {
+    console.log('spara')
   }
 
   render(props) {
     return (
-      <div className="SettingsDiv">
-        <h2>Settings</h2>
-        <p>Choose organisations to listen to</p>
-        {this.state.orgs}
-        
-        <div className="radioInputs">
-        <RadioGroup onChange={ this.onChange } vertical="true">
-        {this.testar()}
-        
-        </RadioGroup>
-        </div>
+      <div>
+        {this.renderOrganisations()}    
       </div>
         );
       }    
 }
 
-export default Settings;
+export default Settings; 
